e9b82f3ffd5ae1b38de7e9b7048b8e72945666ec
Karolina Milczarek <carla@MacBook-Air-Karolina.local>
2021-01-09 13:27:07 +0100

    Initial commit

---
+++ src/main/java/com/sda/java/JavaLdz26/JavaLdz26Application.java
@@ -0,0 +1,13 @@
+package com.sda.java.JavaLdz26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLdz26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLdz26Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/java/JavaLdz26/JavaLdz26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.java.JavaLdz26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLdz26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
